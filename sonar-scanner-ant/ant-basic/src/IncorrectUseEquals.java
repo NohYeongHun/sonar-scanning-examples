d9826e2e0e19518858218ebc299b8ac01b05a8a9
NYH <68585648+NohYeongHun@users.noreply.github.com>
2024-04-22 11:24:40 +0900

    Create IncorrectUseEquals.java

---
+++ sonar-scanner-ant/ant-basic/src/IncorrectUseEquals.java
@@ -0,0 +1,11 @@
+public class IncorrectUseEquals {
+  
+  String str1 = "hello";
+  String str2 = new String("hello");
+  public void Test()
+  {
+    if (str1 == str2) {  // 객체 참조 비교, false
+        // 잘못된 비교 방식, equals() 메서드 사용 권장
+    }
+  }
+}
