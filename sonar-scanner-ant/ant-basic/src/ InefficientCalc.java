66682df6c183c0b86c390609745ae96f7af76619
NYH <68585648+NohYeongHun@users.noreply.github.com>
2024-04-22 11:13:24 +0900

    Update  InefficientCalc.java

--- sonar-scanner-ant/ant-basic/src/ InefficientCalc.java
+++ sonar-scanner-ant/ant-basic/src/ InefficientCalc.java
@@ -1,7 +1,9 @@
 public class InefficientCalc {
-   String result = "";
-   for (int i = 0; i < 1000; i++) {
-       result += i;  // 매 반복마다 새로운 문자열 객체가 생성됨
+   public void Test()
+   {
+      String result = "";
+      for (int i = 0; i < 1000; i++) {
+          result += i;  // 매 반복마다 새로운 문자열 객체가 생성됨
+      }
    }
-
 }
