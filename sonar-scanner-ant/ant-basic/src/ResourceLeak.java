76d82d963b8df62caf533431e2e6ec630cc6b17b
NYH <68585648+NohYeongHun@users.noreply.github.com>
2024-04-22 11:47:10 +0900

    Update ResourceLeak.java

--- sonar-scanner-ant/ant-basic/src/ResourceLeak.java
+++ sonar-scanner-ant/ant-basic/src/ResourceLeak.java
@@ -7,6 +7,7 @@ public class ResourceLeak {
     try {
       FileInputStream inputStream = new FileInputStream("file.txt");
     } catch (IOException e) {
+      // inputStream을 close()하지 않음
       e.printStackTrace();
     }
     
