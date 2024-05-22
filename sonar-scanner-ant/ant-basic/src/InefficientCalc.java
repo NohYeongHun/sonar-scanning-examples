649fb82d2800d7576dd67578fbb6be4edcd0a43c
NYH <68585648+NohYeongHun@users.noreply.github.com>
2024-05-22 11:32:44 +0900

    Update and rename InefficientCalc.java to Nullable.java

--- sonar-scanner-ant/ant-basic/src/InefficientCalc.java
+++
@@ -1,9 +0,0 @@
-public class InefficientCalc {
-   public void Test()
-   {
-      String result = "";
-      for (int i = 0; i < 1000; i++) {
-          result += i;  // 매 반복마다 새로운 문자열 객체가 생성됨
-      }
-   }
-}
---
+++ sonar-scanner-ant/ant-basic/src/Nullable.java
@@ -0,0 +1,12 @@
+public class Nullable {
+
+   private String nullString = null;
+   public void Test()
+   {
+      String testString = "TEST";
+      if (nullString.equals(testString))
+      {
+         // silence
+      }
+   }
+}
