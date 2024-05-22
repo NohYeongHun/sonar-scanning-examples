4a738035b051437ee4479783051c6835e43aff2a
NYH <68585648+NohYeongHun@users.noreply.github.com>
2024-05-22 12:06:55 +0900

    Update and rename Nullable.java to CompareTo.java

---
+++ sonar-scanner-ant/ant-basic/src/CompareTo.java
@@ -0,0 +1,17 @@
+public class CompareTo {
+
+   private String used_amt = "1000000";
+   private String budget_amt = "700000";
+
+   public void Test()
+   {
+      BigDecimal bd_used_amt = new BigDecimal(used_amt);
+      BigDecimal bd_budget_amt = new BigDecimal(budget_amt);
+
+      if (bd_budget_amt.compareTo(bd_used_amt) == -1)
+      {
+        // 사용액과 추가할 예산액 비교 예산액이 큰 경우
+        throw new Exception("예산액이 사용액보다 작습니다.");
+      }
+   }
+}
