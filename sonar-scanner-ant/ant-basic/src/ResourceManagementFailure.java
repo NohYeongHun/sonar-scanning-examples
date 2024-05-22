65d39ebed2285f36e2d080b1f59b622dc1781bbf
NYH <68585648+NohYeongHun@users.noreply.github.com>
2024-05-22 10:47:52 +0900

    Update ResourceManagementFailure.java

--- sonar-scanner-ant/ant-basic/src/ResourceManagementFailure.java
+++ sonar-scanner-ant/ant-basic/src/ResourceManagementFailure.java
@@ -27,7 +27,7 @@ public class ResourceManagementFailure {
         {
            stmt.close();    
         }
-      } catch (IOException e) {
+      } catch (SQLException e) {
         // silence
       }
     }
