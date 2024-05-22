3137a536919a86076261c5c81b63bf773786faa2
NYH <68585648+NohYeongHun@users.noreply.github.com>
2024-05-22 10:55:28 +0900

    Update ResourceManagementFailure.java

--- sonar-scanner-ant/ant-basic/src/ResourceManagementFailure.java
+++ sonar-scanner-ant/ant-basic/src/ResourceManagementFailure.java
@@ -10,24 +10,16 @@ public class ResourceManagementFailure {
   { 
     Connection conn = null;
     Statement stmt = null;
-    try(conn = DriverManager.getConnection(url, user, password)) {
-      try(stmt = conn.createStatement()) {
-        
-      } catch(Exception e) {
+    try (Connection conn = DriverManager.getConnection(url, user, password)) {
+      try (Statement stmt = conn.createStatement()) {
+        // SQL 작업 수행
+      } catch (SQLException e) {
+        // Statement 관련 예외 처리
         e.printStackTrace();
-      } finally {
-        if (stmt != null) {
-          stmt.close();
-        }
       }
-
-    } catch(Exception e) {
-      // Connection 또는 Statement가 적절히 close()되지 않음
+    } catch (SQLException e) {
+      // Connection 관련 예외 처리
       e.printStackTrace();
-    } finally {
-       if (conn != null) {
-         conn.close();
-       }
     }
   }
 }
