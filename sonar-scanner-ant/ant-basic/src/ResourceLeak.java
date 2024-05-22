9993fdf1662baeb7182ff12621c563930a1a4501
NYH <68585648+NohYeongHun@users.noreply.github.com>
2024-05-22 13:45:58 +0900

    Update ResourceLeak.java

--- sonar-scanner-ant/ant-basic/src/ResourceLeak.java
+++ sonar-scanner-ant/ant-basic/src/ResourceLeak.java
@@ -10,16 +10,7 @@ public class ResourceLeak {
     } catch (IOException e) {
       // inputStream을 close()하지 않음
       e.printStackTrace();
-      
-    } finally {  
-      try {
-        if (inputStream !=  null) {
-          inputStream.close();
-        }
-      } catch (IOException e) {
-        //silence.
-      }
-    }
+    } 
     
   }
   
