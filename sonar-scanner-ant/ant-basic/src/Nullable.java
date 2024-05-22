fa659b753906f8e90765b005937da0d6f1336bf5
NYH <68585648+NohYeongHun@users.noreply.github.com>
2024-05-22 11:43:07 +0900

    Update Nullable.java

--- sonar-scanner-ant/ant-basic/src/Nullable.java
+++ sonar-scanner-ant/ant-basic/src/Nullable.java
@@ -1,12 +1,9 @@
 public class Nullable {
 
    private String nullString = null;
+
    public void Test()
    {
-      String testString = "TEST";
-      if (nullString.equals(testString))
-      {
-         // silence
-      }
+      nullString.length();
    }
 }
