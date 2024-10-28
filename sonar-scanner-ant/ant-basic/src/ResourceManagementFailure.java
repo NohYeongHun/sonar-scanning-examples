65cef1ac11dc8c912f49fc77626d7635f0a3dc60
NYH <68585648+NohYeongHun@users.noreply.github.com>
2024-10-28 15:48:52 +0900

    Update ResourceManagementFailure.java

--- sonar-scanner-ant/ant-basic/src/ResourceManagementFailure.java
+++ sonar-scanner-ant/ant-basic/src/ResourceManagementFailure.java
@@ -4,32 +4,32 @@ import java.io.*;
 public class ResourceManagementFailure {
   String url = "";
   String user = "user";
-  String password = "password";
+  // String password = "password";
   
   public void test()
   { 
     Connection conn = null;
     Statement stmt = null;
-    try {
-      conn = DriverManager.getConnection(url, user, password);
-      stmt = conn.createStatement();
-    } catch (Exception e) {
-      e.printStackTrace();
-    } finally {
-      if (stmt != null) {
-        try { 
-          stmt.close(); 
-        } catch(Exception e) {
-          // silence
-        }
-      }
-      if (conn != null) {
-        try { 
-          conn.close(); 
-        } catch(Exception e) {
-          // silence
-        }  
-      }
-    }
-  }
+  //   try {
+  //     conn = DriverManager.getConnection(url, user, password);
+  //     stmt = conn.createStatement();
+  //   } catch (Exception e) {
+  //     e.printStackTrace();
+  //   } finally {
+  //     if (stmt != null) {
+  //       try { 
+  //         stmt.close(); 
+  //       } catch(Exception e) {
+  //         // silence
+  //       }
+  //     }
+  //     if (conn != null) {
+  //       try { 
+  //         conn.close(); 
+  //       } catch(Exception e) {
+  //         // silence
+  //       }  
+  //     }
+  //   }
+  // }
 }
